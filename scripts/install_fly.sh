e9c9c95ae1969efd3bb09a4697a64f35fbbf9b48
Laurent Verbruggen <laurent.verbruggen@kbc.be>
2016-11-25 13:12:45 +0100

    Upgrade fly dependency

--- scripts/install_fly.sh
+++ scripts/install_fly.sh
@@ -7,7 +7,7 @@ _main() {
   tmpdir="$(mktemp -d git_lfs_install.XXXXXX)"
 
   cd "$tmpdir"
-  curl -Lo fly https://github.com/concourse/concourse/releases/download/v2.4.0/fly_linux_amd64
+  curl -Lo fly https://github.com/concourse/concourse/releases/download/v2.5.0/fly_linux_amd64
   mv fly /usr/bin
   chmod 755 /usr/bin/fly
   cd ..
